cfc101a5c3f6c2e1c5021d52eca5af9ef0102d2a
Parav Nagarsheth <parav.nagarsheth@gmail.com>
2022-10-06 18:11:49 +0530

    push script for architext tokenization and data split

--- tools/preprocess_architext_fim.py
+++ tools/preprocess_architext_fim.py
@@ -1,18 +1,5 @@
-# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-"""Processing data for pretraining."""
+"""Processing architext dataset for FIM, and later diffing."""
 
 import argparse
 import multiprocessing
@@ -26,39 +13,12 @@ sys.path.append(
 )
 import time
 import tqdm
+import numpy as np
 import torch
-import ftfy
 
 from megatron.tokenizer import build_tokenizer
 from megatron.data import indexed_dataset
 from threading import Semaphore
-
-def get_train_valid_test_split_(splits_string, size):
-    """Get dataset splits from comma or '/' separated string list."""
-
-    splits = []
-    if splits_string.find(",") != -1:
-        splits = [float(s) for s in splits_string.split(",")]
-    elif splits_string.find("/") != -1:
-        splits = [float(s) for s in splits_string.split("/")]
-    else:
-        splits = [float(splits_string)]
-    while len(splits) < 3:
-        splits.append(0.0)
-    splits = splits[:3]
-    splits_sum = sum(splits)
-    assert splits_sum > 0.0
-    splits = [split / splits_sum for split in splits]
-    splits_index = [0]
-    for index, split in enumerate(splits):
-        splits_index.append(splits_index[index] + int(round(split * float(size))))
-    diff = splits_index[-1] - size
-    for index in range(1, len(splits_index)):
-        splits_index[index] -= diff
-    assert len(splits_index) == 4
-    assert splits_index[-1] == size
-    return splits_index
-
 
 
 class Encoder(object):
@@ -72,14 +32,70 @@ class Encoder(object):
     def encode(self, text):
         if self.args.ftfy:
             text = ftfy.fix_text(text)
-        doc_ids = []
-        text_ids = Encoder.tokenizer.tokenize(text)
-        if len(text_ids) > 0:
-            doc_ids.append(text_ids)
-        if self.args.append_eod:
-            doc_ids[-1].append(Encoder.tokenizer.eod)
-        return doc_ids, len(text)
+        ids = {}
+        for key in self.args.jsonl_keys:
+            doc_ids = []
+            text_ids = Encoder.tokenizer.tokenize(text)
+            if len(text_ids) > 0:
+                doc_ids.append(text_ids)
+            if self.args.append_eod:
+                doc_ids[-1].append(Encoder.tokenizer.eod)
+            ids[key] = doc_ids
+        return ids, len(text)
 
+class FIMer(object):
+    def __init__(self, args):
+        self.args = args
+    
+    def initializer(self, seed_sequence):
+        # use FIMer class as a global rng
+        FIMer.rng = np.random.default_rng(seed_sequence)
+
+    def fim_document(self, text):
+        fim_rate = self.args.fim_rate
+        
+        suffix_token, prefix_token, middle_token = self.args.suffix_token, self.args.prefix_token, self.args.middle_token
+        eod_token = "<|endoftext|>"
+
+        if FIMer.rng.binomial(1, fim_rate):
+            # FIM this document
+            # Remove <EOD> token
+            text = text.replace(eod_token, "")
+
+            # error handling for empty documents/lines
+            try:
+                # Split text into 2 parts
+                prompt, layout = text.split("[layout]")
+                spaces = layout.split(", ")
+                spaces = [s.strip() for s in spaces]
+                if len(spaces) <= 2:
+                    # don't bother masking
+                    return text, len(text)
+                # shuffle the spaces
+                FIMer.rng.shuffle(spaces)
+
+                # sample a number of spaces to mask, 
+                # mask at least one, keep at least one ?
+                num_spaces = FIMer.rng.integers(1, len(spaces)-1)
+
+                # select what contiguous spaces to mask
+                # don't mask the first one
+
+                start_idx = FIMer.rng.integers(1, len(spaces)-num_spaces)
+                end_idx = start_idx + num_spaces
+
+                prefix = prompt + "[layout] " + prefix_token + " " + ", ".join(spaces[:start_idx])
+                middle = middle_token + " " + ", ".join(spaces[start_idx:end_idx])
+                suffix = suffix_token + " " + ", ".join(spaces[end_idx:]) + " "
+
+                # PSM or SPM?
+                # do PSM for now
+                text = " ".join([prefix, suffix, middle, eod_token])
+            except Exception as e:
+                safe_text = ' '.join('{:02X}'.format(c) for c in text.encode())
+                print(f"Error FIMing document {safe_text}: {e}")
+                return '', 0
+        return text, len(text)
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -88,22 +104,16 @@ def get_args():
         "--input",
         type=str,
         required=True,
-        help="Path to input jsonl files or lmd archive(s) - if using multiple archives, put them in a comma separated "
+        help="Path to input files that contain prompts and layouts on each line"
         "list",
     )
     group.add_argument(
         "--num-docs",
         default=None,
-        help="Number of documents in the input data (if known) for an accurate progress bar and train/val/test splits.",
-        required=True,
+        help="Optional: Number of documents in the input data (if known) for an accurate progress bar.",
         type=int,
     )
-    group.add_argument(
-        "--split",
-        default="0.97,0.015,0.015",
-        help="Comma separated list of train, validation and test split sizes.",
-        type=str,
-    )
+
     group = parser.add_argument_group(title="tokenizer")
     group.add_argument(
         "--tokenizer-type",
@@ -127,17 +137,42 @@ def get_args():
         help="Path to the BPE merge file (if necessary).",
     )
     group.add_argument(
+        "--prefix-token",
+        type=str,
+        default="<|prefix|>",
+        help="Token to use for prefix (during FIM).",
+    )
+    group.add_argument(
+        "--middle-token",
+        type=str,
+        default="<|middle|>",
+        help="Token to use for middle (during FIM).",
+    )
+    group.add_argument(
+        "--suffix-token",
+        type=str,
+        default="<|suffix|>",
+        help="Token to use for suffix (during FIM).",
+    )
+    group.add_argument(
         "--append-eod",
         action="store_true",
         help="Append an <eod> token to the end of a document.",
     )
-    group.add_argument("--ftfy", action="store_true", help="Use ftfy to clean text")
+    group.add_argument(
+        "--fim-rate",
+        type=float,
+        default=1.0,
+        help="Rate at which to apply FIM preproc.",
+    )
+
     group = parser.add_argument_group(title="output data")
+
     group.add_argument(
         "--output-prefix",
         type=str,
         required=True,
-        help="Path to binary output file without suffix",
+        help="Path to output file(s) without suffix",
     )
     group.add_argument(
         "--dataset-impl",
@@ -145,6 +180,12 @@ def get_args():
         default="mmap",
         choices=["lazy", "cached", "mmap"],
         help="Dataset implementation to use. Default: mmap",
+    )
+    group.add_argument(
+        "--output-vocabulary-dir",
+        type=str,
+        default=None,
+        help="Optional path to save the vocabulary used to encode the data",
     )
 
     group = parser.add_argument_group(title="runtime")
@@ -181,6 +222,7 @@ def yield_from_files(fnames: list, semaphore):
             semaphore.acquire()
             for doc in f.strip().split('\n'):
                 yield doc
+
     for fname in fnames:
         semaphore.acquire()
 
@@ -201,6 +243,39 @@ def main():
     # use multiprocessing to iterate over input documents
     fin = yield_from_files(args.input.split(","), semaphore)
 
+    seed_seq = np.random.SeedSequence(1234)
+
+    fimer = FIMer(args)
+
+    # import pdb; pdb.set_trace()
+    if args.workers > 1:
+        pool = multiprocessing.Pool(args.workers, initializer=fimer.initializer, initargs=(seed_seq,))
+        fimed_docs = pool.imap(fimer.fim_document, fin, chunksize=25)
+    else:
+        fimer.initializer(seed_seq)
+        fimed_docs = (fimer.fim_document(doc) for doc in fin)
+
+    proc_start = time.time()
+    total_bytes_processed = 0
+    pbar = tqdm.tqdm()
+    with open(f"{args.output_prefix}.txt", "w") as fout:
+        for i, (doc, bytes_processed) in enumerate(fimed_docs, start=1):
+            total_bytes_processed += bytes_processed
+            semaphore.release()
+            if doc:
+                fout.write(doc + "\n")
+            # log progress
+            if i % args.log_interval == 0:
+                current = time.time()
+                elapsed = current - proc_start
+                mbs = total_bytes_processed / elapsed / 1024 / 1024
+                pbar.set_description(
+                    f"Processed {i}{'' if args.num_docs is None else '/' + str(args.num_docs)} documents ({i / elapsed} docs/s, {mbs} MB/s)."
+                )
+                if i != 0:
+                    pbar.update(args.log_interval)
+
+    sys.exit()
     if args.workers > 1:
         pool = multiprocessing.Pool(args.workers, initializer=encoder.initializer)
         encoded_docs = pool.imap(encoder.encode, fin, chunksize=25)
@@ -213,8 +288,7 @@ def main():
     output_bin_files = {}
     output_idx_files = {}
     builders = {}
-    datakeys = ['train', 'valid', 'test']
-    for key in datakeys:
+    for key in args.jsonl_keys:
         output_bin_files[key] = "{}_{}_{}.bin".format(
             args.output_prefix, key, "document"
         )
@@ -226,28 +300,11 @@ def main():
             impl=args.dataset_impl,
             vocab_size=tokenizer.vocab_size,
         )
-    splits = get_train_valid_test_split_(args.split, args.num_docs)
 
-    def print_split_stats(name, index):
-        print("    {}:".format(name))
-        print(
-            "     document indices in [{}, {}) total of {} "
-            "documents".format(
-                splits[index], splits[index + 1], splits[index + 1] - splits[index]
-            )
-        )
-
-    print_split_stats("train", 0)
-    print_split_stats("validation", 1)
-    print_split_stats("test", 2)
-
-    split_index = 0
-    split_key = datakeys[split_index]
     # actually do tokenization
     proc_start = time.time()
     total_bytes_processed = 0
     pbar = tqdm.tqdm()
-    import pdb; pdb.set_trace()
     for i, (doc, bytes_processed) in enumerate(encoded_docs, start=1):
         total_bytes_processed += bytes_processed
 
@@ -255,15 +312,11 @@ def main():
         semaphore.release()
 
         # add each tokenized document / sentence
-        builders[split_key].add_item(torch.IntTensor(doc))
-        # separate with eos token
-        builders[split_key].end_document()
-
-        if split_index < 2 and i == splits[split_index + 1]:
-            # finish up the current split
-            builders[split_key].finalize(output_idx_files[split_key])
-            split_index += 1
-            split_key = datakeys[split_index]
+        for key, sentences in doc.items():
+            for sentence in sentences:
+                builders[key].add_item(torch.IntTensor(sentence))
+            # separate with eos token
+            builders[key].end_document()
 
         # log progress
         if i % args.log_interval == 0:
@@ -276,8 +329,10 @@ def main():
             if i != 0:
                 pbar.update(args.log_interval)
 
-    # finish final split
-    builders[split_key].finalize(output_idx_files[split_key])
+    # save output file
+    for key in args.jsonl_keys:
+        builders[key].finalize(output_idx_files[key])
+
 
 if __name__ == "__main__":
     main()
